69e562075c949e9d1c85dce46b02a4c19d5cf09b
LinaRiddersporre <85893597+LinaRiddersporre@users.noreply.github.com>
2021-11-02 21:37:51 +0100

    Add files via upload

--- 2_nov_practice/js/script.js
+++ 2_nov_practice/js/script.js
@@ -15,21 +15,65 @@ const p = document.createElement('p');
 body.appendChild(p);
 p.innerText = 'Skapa ett nytt p-element och lägg till det i bodyn. Ge elementet text-innehåll. ';
 
-const ol = document.createElement('ol');
-body.appendChild(ol);
+// const ol = document.createElement('ol');
+// body.appendChild(ol);
 
-const li1 = document.createElement('li');
-li1.innerText = 'Skämt 1';
-ol.appendChild(li1);
+// const li1 = document.createElement('li');
+// li1.innerText = 'Skämt 1';
+// ol.appendChild(li1);
 
-const li2 = document.createElement('li');
-li2.innerText = 'Skämt 2';
-ol.appendChild(li2);
+// const li2 = document.createElement('li');
+// li2.innerText = 'Skämt 2';
+// ol.appendChild(li2);
 
-const li3 = document.createElement('li');
-li3.innerText = 'Skämt 3';
-ol.appendChild(li3);
+// const li3 = document.createElement('li');
+// li3.innerText = 'Skämt 3';
+// ol.appendChild(li3);
 
 body.style.textAlign = 'center';
 
 //Loops
+
+for(let i = 0; i < 10; i++) {
+    console.log(i);
+}
+
+for(let i = 111; i < 134; i++) {
+    console.log(i);
+}
+
+for(let i = 20; i > 14; i--) {
+    console.log(i);
+}
+
+//Annan lösning?
+for(let i = 50; i > 0; i--) {
+    
+    console.log(i);
+    i = i-4;
+}
+
+for(let i = 0.0; i <= 100; i++) {
+    
+    console.log(i);
+    i=i*1.3;
+}
+
+const ol = document.createElement('ol');
+body.appendChild(ol);
+
+
+for(let i=0; i < 10; i++){
+    const li = document.createElement('li');
+    ol.appendChild(li);
+     console.log(li);
+
+    li.style.border = 'solid';
+    li.style.borderWidth = `${i+1}px`;
+    li.style.borderColor = 'black';
+
+    li.style.backgroundColor = `hsl(0, 100%, ${100-(12*i)}%`;
+
+    li.innerText = 'Test';
+    li.style.color = `hsl(0, 100%, ${11*i}%`;
+}
